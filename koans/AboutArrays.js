a4e7046cd7ada4abfd3681a614ee636e3bf5e4a6
Sarrah Vesselov <sarchka1@gmail.com>
2015-03-04 16:44:46 -0500

    completed problem 5 and 6

--- koans/AboutArrays.js
+++ koans/AboutArrays.js
@@ -70,18 +70,18 @@ describe("About Arrays", function() {
 
     var copyOfArray = array.slice();
     copyOfArray[3] = "changed in copyOfArray";
-    expect(array[3]).toBe('changed in copyOfArray');
+    expect(array[3]).toBe('three');
   });
 
   it("should push and pop", function () {
     var array = [1, 2];
     array.push(3);
 
-    expect(array).toEqual(FILL_ME_IN);
+    expect(array).toEqual([1, 2, 3]);
 
     var poppedValue = array.pop();
-    expect(poppedValue).toBe(FILL_ME_IN);
-    expect(array).toEqual(FILL_ME_IN);
+    expect(poppedValue).toBe(3);
+    expect(array).toEqual([1, 2]);
   });
 
   it("should know about shifting arrays", function () {
